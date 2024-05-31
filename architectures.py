Update with loss computation correction and minor fix - in line with results obtained on 29th may

architectures.py
@@ -6,40 +6,36 @@ from torch.nn import functional as F
 
 from typing import Dict
 
-#from utils.configs import PARAMS
-
-#I parametri dal parser utilizzati in questo file:
-#   '--dropout', type=float, default=0.5, required=False, help='Dropout rate'
-#   '--fc', type=int, default=128, required=False, help='Size of last FC layer'
-
 #NEW
 import lightning.pytorch as pl
 
 
 class MultiParametricMRIModel(pl.LightningModule): #(nn.Module):
 
-    def __init__(self, FC, dropout):
+    def __init__(self, FC, dropout, backbone="ResNet50"):
         """
-        Each branch is a ResNet50, pre-trained on ImageNet. The head is substituted by a FC (4096,2) followed
-        by a Softmax activation function. The original model seems to output logits instead of soft probs.
+        Each branch is a ResNet50, pre-trained on ImageNet. The head is substituted by a FC (4096,2).
         """
         super(MultiParametricMRIModel, self).__init__()
-        self.branchDWI = BranchModel()
-        self.branchT2 = BranchModel()
-        self.branchDCEpeak = BranchModel()
-        self.branchDCE3TP = BranchModel()
+        self.name = "MultiparametricMRIModel"
+        self.backbone = backbone
+        self.branchDWI = BranchModel(self.backbone, "DWI")
+        self.branchT2 = BranchModel(self.backbone, "T2")
+        self.branchDCEpeak = BranchModel(self.backbone, "DCE_peak")
+        self.branchDCE3TP = BranchModel(self.backbone, "DCE_3TP")
 
-        #Multimodality classifier
+        #Multiparametric classifier
+        features_dimension = 4*self.branchDWI.extracted_features_dim   # TODO - it is better to parametrize the 4 with the number of branches, i.e. len(args.branches)
         self.classifier = nn.Sequential(
             nn.Dropout(p=dropout),
-            nn.Linear(16384, FC), #dimension = 4*4096 = 16384
+            nn.Linear(features_dimension, FC), 
             nn.ReLU(),
             nn.Linear(FC, 2), 
-            nn.Softmax(dim = 1)
+            #nn.Softmax(dim = 1)
         )
 
     def forward(self, x: torch.Tensor) -> Dict:
-        #   (DWI_pre, DWI_post, T2_pre, T2_post,DCEpeak_pre, DCEpeak_post, DCE3TP_pre, DCE3TP_post)
+        #   (DWI_pre, DWI_post, T2_pre, T2_post,DCEpeak_pre, DCEpeak_post, DCE3TP_pre, DCE3TP_post)  # TODO - include the control on the selected branches for the experiments
         x_DWI_1, x_T2_1, x_DCEpeak_1, x_DCE3TP_1 = x[0], x[2], x[4], x[6] 
         x_DWI_2, x_T2_2, x_DCEpeak_2, x_DCE3TP_2 = x[1], x[3], x[5], x[7]
 
@@ -65,21 +61,24 @@ class MultiParametricMRIModel(pl.LightningModule): #(nn.Module):
 
 class BranchModel(pl.LightningModule): #(nn.Module):
 
-    def __init__(self):
+    def __init__(self, backbone, branch_name):
         """
-        Each branch is a ResNet50, pre-trained on ImageNet. The head is substituted by a FC (4096,2) followed
-        by a Softmax activation function. The original model seems to output logits instead of soft probs.
-        #TODO: Analyze the loss function to understand the output of the individual branches.
+        By default each branch is a ResNet50, pre-trained on ImageNet. The head is substituted by a FC (4096,2).
         """
         super(BranchModel, self).__init__()
 
-        self.model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
-        #adding L2 regularization to conv layers
+        self.branch_name = branch_name
+        if backbone == "ResNet50":
+            self.model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
+        else:
+            self.model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
+        
         #conv_layer_params = [param for name, param in self.model.named_parameters() if 'conv' in name]
         
+        self.extracted_features_dim = 2 * self.model.fc.in_features
         self.model.fc = nn.Sequential(
-                nn.Linear(4096,2)
-                #nn.Softmax(dim=1)                       
+                nn.Linear(self.extracted_features_dim,2),
+                #nn.Softmax(dim=1)                     
             )
         
     def forward(self, x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
@@ -93,7 +92,7 @@ class BranchModel(pl.LightningModule): #(nn.Module):
         x1 = self.model.layer4(x1)
         x1 = self.model.avgpool(x1) 
         x1 = x1.view(int(x1.size()[0]),-1)
-        print(f"Extracted features of single modality, PRE nac: {x1.shape}")
+        print(f"Shape of extracted features by branch {self.branch_name}, PRE nac: {x1.shape}")
 
         x2 = self.model.conv1(x2)
         x2 = self.model.bn1(x2)
@@ -105,12 +104,11 @@ class BranchModel(pl.LightningModule): #(nn.Module):
         x2 = self.model.layer4(x2)
         x2 = self.model.avgpool(x2) 
         x2 = x2.view(int(x2.size()[0]),-1)
-        print(f"Extracted features of single modality, POST nac: {x2.shape}")
+        print(f"Shape of extracted features by branch {self.branch_name}, POST nac: {x2.shape}")
 
         conc_features = torch.cat((x1, x2), dim=1)
-        print(f"Shape features single modality concatenated (pre + post NAC): {conc_features.shape}\n")
+        print(f"Shape of concatenated features (branch {self.branch_name}, pre + post NAC): {conc_features.shape}\n")
         probs = self.model.fc(conc_features)
-        #print(f"probs: {probs}\n")
         
         return conc_features, probs
     

dataset_lib.py
@@ -5,28 +5,29 @@ import numpy as np
 from lightning.pytorch.utilities.types import TRAIN_DATALOADERS
 import torch 
 
+from sklearn.utils.class_weight import compute_class_weight
+
 from torch.nn import functional as F
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from torchvision.transforms import v2 as T
 import lightning.pytorch as pl
 
-from utils.dataset_utils import get_transformations_COPY, get_val_transformations, normalize_slice_channelwise, normalize_slices
-
-import logging
+from utils.dataset_utils import get_transformations, get_val_transformations, normalize_slice_channelwise, normalize_slices
 
 branches_list = ["DWI","T2","DCE_peak","DCE_3TP"]
 scans = ["DWI","T2","DCE"]
 
 class MRIDataset(Dataset):
 
-    def __init__(self, folders_list=None, slices = 3, transform = None, preprocess_type = ""):
+    def __init__(self, folders_list=None, slices = 3, transform = None, preprocess_type="norm"):
         
         self.extra_slices_num = slices
         self.branches_list = branches_list
         self.scans = scans
         self.transform = transform
         self.preprocess_type = preprocess_type
+        print(f"Preprocess type : {self.preprocess_type}")
 
         print("1. DEFINING SUBSEQUENCES")
         features, labels, scan_list, extra_slices_per_side, patient_list = self._define_subsequences(folders_list)
@@ -38,8 +39,6 @@ class MRIDataset(Dataset):
         self.scan_list = scan_list
         self.slices = X
         self.labels = Y.float()
-
-        #print(self.class_weights)
         
     def __getitem__(self, index) -> (torch.Tensor):#, torch.Tensor):
         """
@@ -58,8 +57,7 @@ class MRIDataset(Dataset):
             print(images)
 
         if self.transform is not None:
-            images = self.transform(images) 
-            print("Normalize post transformation")
+            images = self.transform(images)
             images = normalize_slices(images)
             
         return images, labels
@@ -86,43 +84,6 @@ class MRIDataset(Dataset):
                     train_folders.append(os.path.join(root,folder))
         return train_folders
 
-    def _preprocess_img(self, img):
-        """
-        This method handles 16bit images and convert them into a proper range for torchvision transform, tailored on 8bit images.
-        """
-        preprocess = self.preprocess_type
-
-        slice = sitk.GetArrayFromImage(img)
-        #slice.astype(np.float32)
-        
-        print(f"ORIGINAL VALUES FOR SLICE --- MIN: {slice.min()} - MAX: {slice.max()}")
-        if preprocess=="min_max":
-            slice = slice/slice.max()
-        elif preprocess=="16bit":
-            slice = slice/(2**16-1)
-        elif preprocess=="10bit":
-            slice = slice/(2**10-1)
-            slice[slice>1.]=1.
-        elif preprocess=="12bit":
-            slice = slice/(2**12-1)
-            slice[slice>1.]=1.
-        elif preprocess=="norm_and_scale":
-            slice = (slice-slice.mean())/slice.std()
-            slice = (slice - slice.min())/(slice.max()-slice.min())
-        elif preprocess=="norm":
-            slice = (slice-slice.mean())/slice.std()
-        elif preprocess=="to_int16":  #malissimo ---> tutte saturate
-            slice = slice.astype(np.int16)
-        elif preprocess=="to_uint8":  #malissimo ---> tutte saturate
-            slice = slice.astype(np.float32) * 255. / slice.max()
-            slice = slice.astype(np.uint8)
-        else:
-            pass #questo ritornerà un uint16, che non è supportato da pytorch
-        print(f"\tPOST VALUES FOR SLICE --- MIN: {slice.min()} - MAX: {slice.max()}\n")
-        
-        print(f"Type: {slice.dtype}")
-        return  slice
-    
     def _define_subsequences(self, folders: "list[str]"):
         """
             sequences è la lista di folders ottenute prima 
@@ -150,7 +111,6 @@ class MRIDataset(Dataset):
 
         sub_sequences_dict = {"DWI_1": DWI_1, "DWI_2": DWI_2, "T2_1": T2_1, "T2_2": T2_2, "T1_1": T1_1, "T1_2": T1_2}
 
-        #labels_file = "/home/cnavilli/tesi_nac/labels/pCR.txt"
         labels_file = "labels/pCR.txt"
 
         try:
@@ -223,15 +183,15 @@ class MRIDataset(Dataset):
                         if (index_instance - extra_slices_per_side) < 0:
                             diff_bound = extra_slices_per_side - (index_instance)
                             upper_bound = min(max_instance, (index_instance + extra_slices_per_side + diff_bound))
-
-                #print(f"Per questa sequenza i bound sono: LB {lower_bound} - INDEX {index_instance} - UB {upper_bound}") 
+                # upper and lower bounds for slices had been set.
+                 
                 for img in images:
                     img_instance = int(img.GetMetaData('0020|0013'))
                     if img_instance >= index_instance and img_instance <= upper_bound:
                         image_count += 1
                         if scan_string != "DCE":   
                             #float_slice = sitk.GetArrayFromImage(img)
-                            slices.append(self._preprocess_img(img))
+                            slices.append(self.preprocess_img(img))
                         else:
                             slices.append(img)
 
@@ -239,7 +199,7 @@ class MRIDataset(Dataset):
                         image_count += 1
                         if scan_string != "DCE":
                             #float_slice = sitk.GetArrayFromImage(img)
-                            slices.append(self._preprocess_img(img))
+                            slices.append(self.preprocess_img(img))
                         else:
                             slices.append(img)
 
@@ -249,8 +209,8 @@ class MRIDataset(Dataset):
             
             if patient_flag:         
                 for i in range(len(slices)):
-                    labels.append(label)            #la stessa label è ripetuta per il numero di fette considerate
-                    scan_list.append(name_string)   #nella lista delle scansioni aggiungo "NAC_1" ecc ecc
+                    labels.append(label)            
+                    scan_list.append(name_string)   
                     paz_list.append(name_num)  
             
             if "DWI" in self.branches_list:  
@@ -274,32 +234,37 @@ class MRIDataset(Dataset):
                     else:
                         T1_2.extend(slices)
 
-        lista_dei_tagli = []
+        modalities = []
         for name,sub_sequence in sub_sequences_dict.items():  
-            lista_dei_tagli.append(name)
+            modalities.append(name)
             features.append(sub_sequence)
 
         sub_sequence_div = (10/len(self.branches_list))/2 
-        #print("Fold:", fold+1)              
-        print("Total DICOM Sequences:", round(sequence_count/sub_sequence_div))
-        print("Total DICOM Index Slices:", round(index_count/sub_sequence_div))
-        print("Total DICOM Selected Slices:", round(image_count/sub_sequence_div), "\n")
+                    
+        #print("Total DICOM Sequences:", round(sequence_count/sub_sequence_div))
+        #print("Total DICOM Index Slices:", round(index_count/sub_sequence_div))
+        #print("Total DICOM Selected Slices:", round(image_count/sub_sequence_div), "\n")
 
-        # Adding the class_weight to account for the class unbalance
-        class_weights_dict = self._class_balance(np.array(labels))
-        print(f"Class weight dictionary: {class_weights_dict}")
-        self.class_weights_tensor = torch.tensor([class_weights_dict[key] for key in sorted(class_weights_dict.keys())], dtype=torch.float32)   
-            
+        # Print the classes balance
+        self._check_class_balance(np.array(labels))
+        
         labels = np.array(labels, dtype=int)
+        print(labels[:,0])
+
+        class_weights = compute_class_weight(class_weight='balanced',classes=np.unique(labels[:,0]),y=labels[:,0])  #le class weights vanno calcolate sull'intero training dataset
+        self.class_weights_tensor = torch.tensor(class_weights, dtype=torch.float32)
+        print(f"Class weights: {self.class_weights_tensor}")
+            
+        
         labels = torch.from_numpy(labels).to(torch.float32) 
-        print(f"Labels dtype: {labels.dtype}")
-        print(f"Channels: {lista_dei_tagli}")
+        #print(f"Labels dtype: {labels.dtype}")
+        print(f"Modalities: {modalities}")
         print(f"Scan list: {scan_list}")
         print(f"Extra slice considered: {extra_slices_per_side} - total subsequence lenght: {extra_slices_per_side*2+1}")
 
-        return features, labels, scan_list, extra_slices_per_side, paz_list           
+        return features, labels, scan_list, extra_slices_per_side, paz_list           #scan_list = NAC_1, NAC_1,....per ogni foto
 
-    def _class_balance(self, labels_array: np.array):
+    def _check_class_balance(self, labels_array: np.array):
         """
         Count the frequences of the two classes.
         Parameters:
@@ -315,7 +280,42 @@ class MRIDataset(Dataset):
                 counter_0+=1
             else:
                 counter_1+=1
-        return {'0':counter_0/(k*2+1), '1':counter_1/(k*2+1)}
+
+        class_weights_dict = {'0':counter_0/(k*2+1), '1':counter_1/(k*2+1)}
+        print(f"Dataset balance (label: count): ",end="")
+        for lab in class_weights_dict:
+            print(f"{lab}: {class_weights_dict[lab]};", end=" ")  
+
+    def preprocess_img(self, img):
+        """
+        This method handles 16bit images and convert them into a proper range for torchvision transforms.
+        """
+        preprocess = self.preprocess_type
+        slice = sitk.GetArrayFromImage(img)
+        
+        #print(f"ORIGINAL VALUES FOR SLICE --- MIN: {slice.min()} - MAX: {slice.max()}")
+        if preprocess=="min_max":
+            slice = slice/slice.max()
+        elif preprocess=="16bit":
+            slice = slice/(2**16-1)
+        elif preprocess=="10bit":
+            slice = slice/(2**10-1)
+            slice[slice>1.]=1.
+        elif preprocess=="12bit":
+            slice = slice/(2**12-1)
+            slice[slice>1.]=1.
+        elif preprocess=="percentile":
+            percentiles = [1,99]
+            pmin, pmax = np.percentile(slice, percentiles)
+            slice = (slice - pmin) / (pmax - pmin)
+            slice[slice<0] = 0.
+            slice[slice>1] = 1.
+        else:
+            # norm and scale
+            slice = (slice-slice.mean())/slice.std()
+            slice = (slice - slice.min())/(slice.max()-slice.min())
+
+        return  slice
     
     def _define_DCE(self, T1_1, T1_2):
         new_patient = ""
@@ -367,18 +367,18 @@ class MRIDataset(Dataset):
 
                 if "DCE_peak" in self.branches_list:
                     if patient_name == patient and time == DCE_peak:
-                        DCE_peak1.append(self._preprocess_img(img))
+                        DCE_peak1.append(self.preprocess_img(img))
 
                 if "DCE_3TP" in self.branches_list:
                     if patient_name == patient:
                         if time == DCE_pre:
-                            pre_array.append(self._preprocess_img(img))
+                            pre_array.append(self.preprocess_img(img))
                             DCE_count += 1
                         elif time == DCE_peak:
-                            peak_array.append(self._preprocess_img(img))
+                            peak_array.append(self.preprocess_img(img))
                             DCE_count += 1
                         else:
-                            post_array.append(self._preprocess_img(img))
+                            post_array.append(self.preprocess_img(img))
                             DCE_count += 1
                         if DCE_count == slices*3:
                             for i in range(slices):
@@ -421,18 +421,18 @@ class MRIDataset(Dataset):
 
                 if "DCE_peak" in self.branches_list:
                     if patient_name == patient and time == DCE_peak:
-                        DCE_peak2.append(self._preprocess_img(img))
+                        DCE_peak2.append(self.preprocess_img(img))
 
                 if "DCE_3TP" in self.branches_list:
                     if patient_name == patient:
                         if time == DCE_pre:
-                            pre_array.append(self._preprocess_img(img))
+                            pre_array.append(self.preprocess_img(img))
                             DCE_count += 1
                         elif time == DCE_peak:
-                            peak_array.append(self._preprocess_img(img))
+                            peak_array.append(self.preprocess_img(img))
                             DCE_count += 1
                         else:
-                            post_array.append(self._preprocess_img(img))
+                            post_array.append(self.preprocess_img(img))
                             DCE_count += 1
                         if DCE_count == slices*3:
                             for i in range(slices):
@@ -452,7 +452,7 @@ class MRIDataset(Dataset):
                 features.append(DCE_feature)
         print(f"CI SONO {len(features)} MODALITA''''''")
 
-        X = torch.Tensor()
+        
         modalities = {0: 'DWI pre-NAC',
                       1: 'DWI post-NAC',
                       2: 'T2 pre-NAC',
@@ -461,19 +461,18 @@ class MRIDataset(Dataset):
                       5: 'DCE_peak pre-NAC',
                       6: 'DCE_3TP pre-NAC',
                       7: 'DCE_3TP pre-NAC'}
-        
+        X = torch.Tensor()
         for i,feature in enumerate(features): 
-            print(f"{modalities[i]}:") #TODO da rimuovere
-            feature = torch.tensor(feature)
-            if feature.any():
-                X_sub = feature
-                if feature.shape[1] == 1:
+            feature = np.transpose(feature, (0, 1, 3, 2))    # NB come nell'originale sono stati invertiti H e W --> original: X_sub = feature #np.transpose(feature, (0, 3, 2, 1)) 
+            X_sub = torch.Tensor(feature)
+            if X_sub.any():
+                if X_sub.shape[1] == 1:
                     X_sub = torch.repeat_interleave(X_sub,repeats = 3, dim=1)
                 X_sub = X_sub.to(torch.float32).unsqueeze(0) 
-                X = torch.cat([X, X_sub], dim = 0)
-                print(f"Shape check - line 459 in define_input: {X.shape}")
 
-        Y = F.one_hot(labels.to(torch.int64), 2) #config.NB_CLASSES)
+                X = torch.cat([X, X_sub], dim = 0)
+
+        Y = F.one_hot(labels.to(torch.int64), 2) # 2 == number of classes , final neurons.
 
         print(f"X shape: {X.shape}")
         print(f"Y shape: {Y.shape}")
@@ -483,19 +482,20 @@ class MRIDataset(Dataset):
     
 class MRIDataModule(pl.LightningDataModule):
 
-    def __init__(self, training_folders, validation_folders, slices, batch_size, num_workers = 0):
+    def __init__(self, training_folders, validation_folders, slices, batch_size, num_workers = 0, preprocess="norm_and_scale"):
         super().__init__()
-        #self.training_folders = training_folders
-        #self.validation_folders = validation_folders
-        #self.slices = slices
+        self.training_folders = training_folders
+        self.validation_folders = validation_folders
+        self.slices = slices
         self.batch_size = batch_size
         self.num_workers = num_workers
-        self.train_transformations = get_transformations_COPY()
+        self.preprocess = preprocess
+        self.train_transformations = get_transformations()
         self.val_transformations = get_val_transformations()
 
-        self.train_dataset = MRIDataset(training_folders, slices, transform = self.val_transformations)
+        self.train_dataset = MRIDataset(training_folders, slices, transform = self.train_transformations, preprocess_type=self.preprocess)
         self.class_weights = self.train_dataset.class_weights_tensor    #class weight is computed only over the training dataset, and is used in the computation of the training loss ONLY
-        self.validation_dataset = MRIDataset(validation_folders, slices, transform=self.val_transformations)
+        self.validation_dataset = MRIDataset(validation_folders, slices, transform = self.val_transformations, preprocess_type=self.preprocess)
         self.test_dataset = self.validation_dataset
 
     # def setup(self, stage):
@@ -515,7 +515,7 @@ class MRIDataModule(pl.LightningDataModule):
     def val_dataloader(self):
         return DataLoader(
             dataset=self.validation_dataset,
-            batch_size=self.batch_size,
+            batch_size=len(self.validation_dataset),
             num_workers=self.num_workers,
             shuffle=False
         )
@@ -523,7 +523,7 @@ class MRIDataModule(pl.LightningDataModule):
     def test_dataloader(self):
         return DataLoader(
             dataset=self.test_dataset,
-            batch_size=self.batch_size,
+            batch_size=len(self.test_dataset),
             num_workers=self.num_workers,
             shuffle=False
         )

model.py
@@ -13,9 +13,10 @@ from utils.callbacks_utils import get_LR_scheduler
 
 class LightningModel(pl.LightningModule):
 
-    def __init__(self, num_slices = 3, fc_dimension = 128, dropout = 0.5, exp_name="default", optim = "sgd", lr =0.0001, wd=0.001, secondary_weight=0.2, class_weights=None, folder_time='', fold_num=1):
+    def __init__(self, num_slices = 3, fc_dimension = 128, dropout = 0.5, exp_name="default", backbone="ResNet50", optim = "sgd", lr =0.0001, wd=0.001, secondary_weight=0.2, class_weights=None, folder_time='', fold_num=1):
         super().__init__()
-        self.exp_name = exp_name,
+        self.exp_name = exp_name
+        self.backbone = backbone
         self.num_slices = num_slices
         self.optimizer = optim
         self.lr= lr
@@ -23,18 +24,20 @@ class LightningModel(pl.LightningModule):
         self.secondary_weight = secondary_weight
         self.folder_time = folder_time
         self.fold_num = fold_num
-        print(dropout)
-        print(type(dropout))
 
         if exp_name == "colorization":
             self.model = None
         else:
-            self.model = MultiParametricMRIModel(fc_dimension, dropout)
+            self.model = MultiParametricMRIModel(fc_dimension, dropout, self.backbone)
 
-        self.weights = class_weights.to(self.device)
+        if class_weights is not None: 
+            self.weights = class_weights.to(self.device)
+        else:
+            self.weights = None
+
         self.loss_fn = compute_loss
 
-        # built-in metrics
+        # torchmetrics metrics - auroc may differ from the one computed with sklearn
         self.roc = torchmetrics.ROC(task="binary")
         self.auroc = torchmetrics.AUROC(task="binary")
         self.binary_accuracy = torchmetrics.Accuracy(task="binary")
@@ -57,14 +60,15 @@ class LightningModel(pl.LightningModule):
         return probs
     
     def configure_optimizers(self):
-        #if self.optimizer == "sgd":
+        #if self.optimizer == "sgd": #TODO da capire se ci sono più optimizer come fare
         optimizer =  torch.optim.SGD(
             self.parameters(),
             lr = self.lr,
             weight_decay=self.wd,
             momentum=0.9
         )
-        scheduler = get_LR_scheduler(optimizer) 
+        scheduler = get_LR_scheduler(optimizer) #lo puoi passare solo se c'è un moniotrer del lr
+        
         optimizer_dict = {"optimizer": optimizer, 
                 "lr_scheduler": {
                     "scheduler": scheduler,
@@ -74,15 +78,15 @@ class LightningModel(pl.LightningModule):
         return optimizer_dict
     
     def loss_function(self, probs, labels, weights):
-        loss = self.loss_fn(probs, labels, self.secondary_weight, type='bce', weights=weights)
+        loss = self.loss_fn(probs, labels, self.secondary_weight, type="cross_entropy", weights=weights)
         return loss
     
     def _common_step(self, batch, batch_idx, train=False):
-        # if train:
-        #     weights = self.weights.to(self.device)
-        # else:
-        #     weights = None
-        weights = self.weights.to(self.device) #use always the weight computed on the training
+        if train and self.weights is not None:
+            weights = self.weights.to(self.device)
+        else:
+            weights = None
+        
         images, labels = batch
         images = images.permute(dims=(1,0, *range(2, images.dim()))).to(self.device)
         labels = labels.squeeze(dim=1).to(self.device)
@@ -96,10 +100,10 @@ class LightningModel(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         x,_ = batch
         loss, probs, labels = self._common_step(batch, batch_idx, train=True)
-        accuracy = self.binary_accuracy(probs["pCR"], labels)
-        auroc = self.auroc(probs["pCR"], labels)
+        accuracy = self.binary_accuracy(probs["pCR"].argmax(axis=-1), labels.argmax(axis=-1)) #TODO aggiunto 29 05 alle 17 33
+        auroc = self.auroc(probs["pCR"][:,1], labels[:,1])
         self.log_dict({"train_loss": loss, "train_acc":accuracy, "train_auroc":auroc}, 
-                 on_step=False, on_epoch=True, prog_bar=True, logger=True)    #il logger (es Tensorboard) mostrerà solo la loss, a meno che non ritorno il log_dict con anche le accuracy
+                 on_step=False, on_epoch=True, prog_bar=True, logger=True) 
         output = {'loss':loss, 'probs': probs, 'labels': labels}
         self.training_step_outputs.append(output)
         
@@ -107,15 +111,14 @@ class LightningModel(pl.LightningModule):
         if batch_idx == 1:
             x = x[:4][0]
             grid = torchvision.utils.make_grid(x.view(-1,3,224,224))
-            #self.logger.experiment.add_image("MRIs", grid, self.global_step) Questo va bene per tensorboard ma non per wandb
             self.logger.experiment.log({"MRIs": [wandb.Image(grid, caption = "MRIs")]})
 
         return output
     
     def validation_step(self, batch, batch_idx):
         loss, probs, labels = self._common_step(batch, batch_idx)
-        accuracy = self.binary_accuracy(probs["pCR"], labels)
-        auroc = self.binary_accuracy(probs["pCR"], labels)
+        accuracy = self.binary_accuracy(probs["pCR"].argmax(axis=-1), labels.argmax(axis=-1)) #TODO aggiunto 29 05 alle 17 33
+        auroc = self.auroc(probs["pCR"][:,1], labels[:,1])
         self.log_dict({"val_loss": loss, "val_acc":accuracy, "val_auroc":auroc}, 
                       on_step=False, on_epoch=True, prog_bar=True, logger=True)
 
@@ -125,8 +128,8 @@ class LightningModel(pl.LightningModule):
     
     def test_step(self, batch, batch_idx):
         loss, probs, labels = self._common_step(batch, batch_idx)
-        accuracy = self.binary_accuracy(probs["pCR"], labels)
-        auroc = self.binary_accuracy(probs["pCR"], labels)
+        accuracy = self.binary_accuracy(probs["pCR"].argmax(axis=-1), labels.argmax(axis=-1)) #TODO aggiunto 29 05 alle 17 33
+        auroc = self.auroc(probs["pCR"][:,1], labels[:,1])
         self.log_dict({"test_loss": loss, "test_acc":accuracy, "test_auroc":auroc}, 
                       on_step=False, on_epoch=True, prog_bar=True, logger=True)
 
@@ -137,52 +140,37 @@ class LightningModel(pl.LightningModule):
     # Hooks for on_epoch = True
     def on_train_epoch_end(self):
         #retrieve all the outputs from the epoch steps
-        outputs = self.training_step_outputs
+        # outputs = self.training_step_outputs
         
-        total_labels = torch.Tensor().to(self.device)
-        total_probs = { }
-        for step_dict in outputs:
-            total_labels = torch.concat((total_labels, step_dict['labels']))
-            probs_dict = step_dict["probs"]
-            for k,v in probs_dict.items():
-                if k not in total_probs:
-                    total_probs[k] = torch.Tensor().to(self.device)
-                total_probs[k] = torch.concat((total_probs[k],probs_dict[k]))
+        # total_labels = torch.Tensor().to(self.device)
+        # total_probs = { }
+        # for step_dict in outputs:
+        #     total_labels = torch.concat((total_labels, step_dict['labels']))
+        #     probs_dict = step_dict["probs"]
+        #     for k,v in probs_dict.items():
+        #         if k not in total_probs:
+        #             total_probs[k] = torch.Tensor().to(self.device)
+        #         total_probs[k] = torch.concat((total_probs[k],probs_dict[k]))
         #ora devo mettere le total_probs da qualche parte, se no cosa se ne fa?
         
         #clear the outputs from all the steps
         self.training_step_outputs = []
 
     def on_validation_epoch_end(self) -> None:
-        outputs = self.validation_step_outputs
+        # outputs = self.validation_step_outputs
         
-        total_labels = torch.Tensor().to(self.device)
-        total_probs = { }
-        for step_dict in outputs:
-            total_labels = torch.concat((total_labels, step_dict['labels']))
-            probs_dict = step_dict["probs"]
-            for k,v in probs_dict.items():
-                if k not in total_probs:
-                    total_probs[k] = torch.Tensor().to(self.device)
-                total_probs[k] = torch.concat((total_probs[k],probs_dict[k]))
+        # total_labels = torch.Tensor().to(self.device)
+        # total_probs = { }
+        # for step_dict in outputs:
+        #     total_labels = torch.concat((total_labels, step_dict['labels']))
+        #     probs_dict = step_dict["probs"]
+        #     for k,v in probs_dict.items():
+        #         if k not in total_probs:
+        #             total_probs[k] = torch.Tensor().to(self.device)
+        #         total_probs[k] = torch.concat((total_probs[k],probs_dict[k]))
         
         # #clean the outputs
         self.validation_step_outputs = []
-
-    # def on_test_epoch_end(self) -> None:  ----probabilmente questo step è superfluo
-    #     #outputs è una lista di dictionaries
-    #     # {loss: single_value, probs: dizionario, labels: torch.Tensor}
-    #     outputs = self.test_step_outputs
-        
-    #     total_labels = torch.Tensor().to(self.device)
-    #     total_probs = { }
-    #     for step_dict in outputs:
-    #         total_labels = torch.concat((total_labels, step_dict['labels']))
-    #         probs_dict = step_dict["probs"]
-    #         for k,v in probs_dict.items():
-    #             if k not in total_probs:
-    #                 total_probs[k] = torch.Tensor().to(self.device)
-    #             total_probs[k] = torch.concat((total_probs[k],probs_dict[k]))
 
     def on_test_end(self):
         print("\n\nON TEST END")
@@ -197,16 +185,21 @@ class LightningModel(pl.LightningModule):
                 if k not in total_probs:
                     total_probs[k] = torch.Tensor().to(self.device)
                 total_probs[k] = torch.concat((total_probs[k],probs_dict[k]))
+        
+        #Calcolo delle metriche con torchvision
+        auroc = self.auroc(total_probs["pCR"], total_labels)
+        print(f"AUC calcolata con torchmetrics: {auroc}")
 
         # Calcolo delle metriche sulla singola fold
         for output_name, Y_prob in total_probs.items():
             print(output_name, " : ", Y_prob)
             roc_result_slice = test_predict(total_labels, Y_prob, "slice", output_name, self.folder_time, self.fold_num, self.num_slices)
-            print(roc_result_slice)
+            #print(roc_result_slice)
             self.slice_dict_test[output_name] = roc_result_slice
             Y_val_split, Y_prob_split = get_patient_level(total_labels, Y_prob, self.num_slices)
             roc_result_patient = test_predict(Y_val_split, Y_prob_split, "patient", output_name, self.folder_time, self.fold_num, self.num_slices)
             self.patient_dict_test[output_name] = roc_result_patient
         
-        print("Line 245 di model.py")
+        print("Dizionario con le probabilità a livello slice: ")
         print(self.slice_dict_test)
+        self.test_step_outputs = []

train.py
@@ -6,13 +6,14 @@ import numpy as np
 import lightning.pytorch as pl
 from lightning.pytorch.loggers import TensorBoardLogger, WandbLogger
 
-from utils.configurazioni import parse_arguments
-from utils.callbacks_utils import get_callbacks
-from utils.train_utils import retrieve_folders_list, Kfold_split, print_results
+from utils.configurations import parse_arguments
+from utils.callbacks_utils import get_callbacks, get_LR_scheduler
+from utils.train_utils import retrieve_folders_list, Kfold_split, print_results, export_result_as_df
 from model import LightningModel
 from dataset_lib import MRIDataModule
 
 if __name__ =="__main__":
+    print("Start")
     args = parse_arguments()
     print(args.dropout)
     print(args.slices)
@@ -22,11 +23,6 @@ if __name__ =="__main__":
     os.environ['PYTHONHASHSEED'] = str(args.seed)
     torch.manual_seed(args.seed)
     np.random.seed(args.seed)
-
-    #dataset_relative_path = args.input_path
-    #current_dir = os.path.dirname(os.path.abspath(__file__))
-    #dataset_folder = os.path.join(current_dir, dataset_relative_path )
-    #print(dataset_folder)
     
     folders_list = retrieve_folders_list(args.input_path)
     folder_time = args.exp_name + datetime.now().strftime('%Y-%m-%d_%H-%M-%S-%f')[:-3]
@@ -43,26 +39,34 @@ if __name__ =="__main__":
         fold_start = datetime.now()
         
         #logger = TensorBoardLogger(f"tb_logs_{i}", name="mri_model_v0")
-        logger_wandb = WandbLogger(save_dir= f"wandb_logs_{i}" , name=f"mri_model_MINI_fold_{i+1}", project="NACtry")
-        dm = MRIDataModule(fold[0], fold[1], slices=args.slices, batch_size=args.batch)
+        logger_wandb = WandbLogger(save_dir= f"wandb_logs_{i}" , name=f"preprocess_{args.preprocess}_fold_{i+1}", project="NACtry")
+        dm = MRIDataModule(fold[0], fold[1], slices=args.slices, batch_size=args.batch, preprocess=args.preprocess)
+        
         print(f"CLASS WEIGHTS: {dm.class_weights}")
-        model = LightningModel(args.slices, args.fc, args.dropout, args.exp_name, args.optim, args.learning_rate, args.l2_reg, args.secondary_weight, dm.class_weights, folder_time, i)
+        if args.class_weight == 1:
+            class_weights = dm.class_weights
+        else:
+            class_weights = None
 
-        cb_list = get_callbacks(checkpoint = True, earlystop = True, lr_monitor = True)
+        model = LightningModel(args.slices, args.fc, args.dropout, args.exp_name, args.backbone, args.optim, args.learning_rate, args.l2_reg, args.secondary_weight, class_weights, folder_time, i)
+
+        cb_list = get_callbacks(checkpoint = True, earlystop = False, lr_monitor = True)
 
         #Istantiate a trainer 
         trainer = pl.Trainer(
-            logger = logger_wandb, #[logger, logger_wandb]
+            logger = logger_wandb,
             accelerator = 'auto',
             default_root_dir='./LOGS',
             num_sanity_val_steps=0,
-            precision='bf16',
+            precision='bf16-mixed', #bf16',
             min_epochs=1,
             max_epochs=args.epochs,
             check_val_every_n_epoch=1,
-            callbacks=cb_list, 
+            callbacks=cb_list,  
             reload_dataloaders_every_n_epochs=1
         )
+        
+        
 
         print(f"LR: {args.learning_rate}, WD: {args.l2_reg}")
         trainer.fit(model=model, datamodule=dm)
@@ -70,7 +74,7 @@ if __name__ =="__main__":
         
         ckpt_best = cb_list[0].best_model_path
         print(f"Best checkpoint path: {ckpt_best}")
-
+        
         trainer.test(model=model, datamodule=dm, ckpt_path=ckpt_best)
         wandb.finish()
 
@@ -82,11 +86,9 @@ if __name__ =="__main__":
         print(f"\n\nFOLD {i+1} TIME: {datetime.now()-fold_start}")
     
     #End of the Kfold
-    print("Linea 72")
-    print(f"ROC SLICE:\n{roc_slice}")
-    print_results(roc_slice, roc_patient)
-    print("Linea 74")
-    print()
+    print("CV concluded. Writing the results on file...")
+    output_file_name = export_result_as_df(roc_slice, roc_patient, args)
+    print(f"Results are available inside the file {output_file_name}")
 
 
 

utils/callbacks_utils.py
@@ -1,7 +1,7 @@
 from lightning.pytorch.callbacks import ModelCheckpoint, EarlyStopping, LearningRateMonitor
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
-checkpoint_filepath=""
+checkpoint_filepath="checkpoint"
 
 def get_callbacks(checkpoint: bool = True, earlystop: bool = True, lr_monitor: bool = True) -> list:
     """
@@ -19,7 +19,8 @@ def get_callbacks(checkpoint: bool = True, earlystop: bool = True, lr_monitor: b
     if checkpoint:
         checkpoint_cb = ModelCheckpoint(
             filename = checkpoint_filepath,
-            monitor='val_loss',
+            monitor='val_auroc',
+            mode='max',
             save_weights_only=True,
             save_top_k=1,
             verbose=True
@@ -29,8 +30,9 @@ def get_callbacks(checkpoint: bool = True, earlystop: bool = True, lr_monitor: b
     if earlystop:
         earlystop_cb = EarlyStopping(
             monitor="val_loss",
+            mode="min",
             patience=10,
-            #restore_best_weights=True, -> da trovare l'equivalente di questo
+            min_delta=0.000025,
             verbose=True
         )
         cb_list.append(earlystop_cb)
@@ -54,6 +56,7 @@ def get_LR_scheduler(optimizer):
     """
     lr_scheduler = ReduceLROnPlateau(
         optimizer=optimizer,
+        mode = "min",
         factor=0.5,
         patience=5,
         threshold=0.000025,

utils/computation_utils.py
@@ -3,6 +3,7 @@ from typing import Dict
 
 import os
 import torch
+import torch.nn as nn
 from torch.nn.functional import binary_cross_entropy_with_logits
 import numpy as np
 from torchvision.ops import sigmoid_focal_loss
@@ -13,76 +14,8 @@ import plotly.graph_objs as go
 accepted_branches = ["DWI","T2","DCE_peak","DCE_3TP"]
 branches_list = ["DWI","T2","DCE_peak","DCE_3TP"]
 
-# def load_all_validation_and_compute(model, val_loader, device = None):
-#     """
-#     Da provare se funziona....
-#     """
-#     Y_prob = torch.Tensor()
-#     Y_test = torch.Tensor()
 
-#     if device is None:
-#         device = torch.device("cpu")
-#     model = model.eval()
-
-#     for idx, (features, labels) in enumerate(val_loader):
-#         features, labels = features.to(device), labels.to(device)
-#         features = features.permute(dims=(1,0, *range(2, features.dim())))
-#         labels = labels.squeeze(dim=1)
-
-#         with torch.no_grad():
-#             logits = model(features)
-        
-#         #Y_prob = torch.cat([Y_prob, logits.unsqueeze(0)])
-#         Y_test = torch.cat([Y_test, labels.unsqueeze(0)])
-    
-#     return Y_prob, Y_test
-
-def load_all_validation(val_loader, device = None):
-    """
-  
-    """
-    Y_prob = torch.Tensor()
-    Y_test = torch.Tensor()
-
-    if device is None:
-        device = torch.device("cpu")
-
-    for idx, (features, labels) in enumerate(val_loader):
-        features, labels = features.to(device), labels.to(device)
-        features = features.permute(dims=(1,0, *range(2, features.dim())))
-        labels = labels.squeeze(dim=1)
-        
-        Y_prob = torch.cat([Y_prob, features.unsqueeze(0)])
-        Y_test = torch.cat([Y_test, labels.unsqueeze(0)])
-    
-    return Y_prob, Y_test
-
-def compute_accuracy(model, dataloader, device = None):
-    """
-    Questo  è il metodo sul tutorial di pytorch / ligthningtorch -> devo riadattarlo al mio.
-    
-    """
-    if device is None:
-        device = torch.device("cpu")
-    model = model.eval()
-
-    correct = 0.0
-    total_examples = 0
-
-    for idx, (featues, labels) in enumerate(dataloader):
-        features, labels = features.to(device), labels.to(device)
-
-        with torch.no_grad():
-            logits = model(features)
-        
-        predictions = torch.argmax(logits, dim=1)
-        compare = labels == predictions
-        correct += torch.sum(compare)
-        total_examples += len(compare)
-
-    return correct/total_examples
-
-def compute_loss(y_pred_dict: Dict, y_true: torch.Tensor, secondary_weight: float = 0.2, type="bce", weights = None):
+def compute_loss(y_pred_dict: Dict, y_true: torch.Tensor, secondary_weight: float = 0.2, type="cross_entropy", weights = None):
     """
     Compute the loss function considering all the different branches and the corresponding weights.
 
@@ -101,23 +34,35 @@ def compute_loss(y_pred_dict: Dict, y_true: torch.Tensor, secondary_weight: floa
     y_pred_pCR = y_pred_dict["pCR"]
 
     #print(f"Check where the tensors are: Y_pred_DWI: {y_pred_DWI.device} | y_true: {y_true.device}")
-    if weights is not None:
-        pos_weight = torch.Tensor([weights[0]/weights[1], weights[1]/weights[0]]).to(weights.device) #nel caso funzionasse meglio, questo lo faccio già nei weights che passo in firma.
-    else:
-        pos_weight = None
-
+    
     if type == "bce":
-        loss_DWI = binary_cross_entropy_with_logits(y_pred_DWI, y_true, reduction="mean", pos_weight=pos_weight )#weight=weights) 
-        loss_T2 = binary_cross_entropy_with_logits(y_pred_T2, y_true, reduction="mean", pos_weight=pos_weight)#weight=weights)
-        loss_DCEpeak = binary_cross_entropy_with_logits(y_pred_DCEpeak, y_true, reduction="mean", pos_weight=pos_weight) #weight=weights)
-        loss_DCE3TP = binary_cross_entropy_with_logits(y_pred_DCE3TP, y_true, reduction="mean", pos_weight=pos_weight)#weight=weights)
-        loss_pcr = binary_cross_entropy_with_logits(y_pred_pCR, y_true, reduction="mean", pos_weight=pos_weight)#weight=weights)
+
+        if weights is not None:
+            pos_weight = torch.Tensor([weights[1]/weights[0], weights[0]/weights[1]]).to(weights.device)
+        else:
+            pos_weight = None
+        loss_DWI = binary_cross_entropy_with_logits(y_pred_DWI, y_true, reduction="mean", pos_weight=pos_weight )
+        loss_T2 = binary_cross_entropy_with_logits(y_pred_T2, y_true, reduction="mean", pos_weight=pos_weight)
+        loss_DCEpeak = binary_cross_entropy_with_logits(y_pred_DCEpeak, y_true, reduction="mean", pos_weight=pos_weight) 
+        loss_DCE3TP = binary_cross_entropy_with_logits(y_pred_DCE3TP, y_true, reduction="mean", pos_weight=pos_weight)
+        loss_pcr = binary_cross_entropy_with_logits(y_pred_pCR, y_true, reduction="mean", pos_weight=pos_weight)
+
+    elif type =="cross_entropy":
+        ce_loss = torch.nn.CrossEntropyLoss(weight=weights,reduction='mean')
+
+        loss_DWI = ce_loss(y_pred_DWI, y_true.argmax(axis=-1))
+        loss_T2 = ce_loss(y_pred_T2, y_true.argmax(axis=-1))
+        loss_DCEpeak = ce_loss(y_pred_DCEpeak, y_true.argmax(axis=-1))
+        loss_DCE3TP = ce_loss(y_pred_DCE3TP, y_true.argmax(axis=-1))
+        loss_pcr = ce_loss(y_pred_pCR, y_true.argmax(axis=-1))
+
     else:
-        loss_DWI = sigmoid_focal_loss(y_pred_DWI, y_true, reduction="mean") 
-        loss_T2 = sigmoid_focal_loss(y_pred_T2, y_true, reduction="mean")
-        loss_DCEpeak = sigmoid_focal_loss(y_pred_DCEpeak, y_true, reduction="mean")
-        loss_DCE3TP = sigmoid_focal_loss(y_pred_DCE3TP, y_true, reduction="mean")
-        loss_pcr = sigmoid_focal_loss(y_pred_pCR, y_true, reduction="mean")
+        return torch.tensor([100.], dtype = torch.float32)
+        # loss_DWI = sigmoid_focal_loss(y_pred_DWI, y_true, reduction="mean") 
+        # loss_T2 = sigmoid_focal_loss(y_pred_T2, y_true, reduction="mean")
+        # loss_DCEpeak = sigmoid_focal_loss(y_pred_DCEpeak, y_true, reduction="mean")
+        # loss_DCE3TP = sigmoid_focal_loss(y_pred_DCE3TP, y_true, reduction="mean")
+        # loss_pcr = sigmoid_focal_loss(y_pred_pCR, y_true, reduction="mean")
 
     print(f"loss_DWI: {loss_DWI}")
     print(f"loss_T2: {loss_T2}")
@@ -162,7 +107,6 @@ def get_patient_level(Y_test: torch.Tensor, Y_prob: torch.Tensor, slices: int) -
     split = torch.split(Y_test_cpu, sequence_length)
 
     for slice in split:
-        #all_equal = all(pCR == slice.argmax(axis=-1)[0] for pCR in slice.argmax(axis=-1))   #tutte le labels dello stesso paziente devono essere uguali
         all_equal = all(pCR == torch.max(slice, dim=-1).indices[0] for pCR in torch.max(slice, dim=-1).indices)
         if all_equal:
             Y_test_split = torch.concat((Y_test_split, slice[0].unsqueeze(0)))
@@ -172,8 +116,7 @@ def get_patient_level(Y_test: torch.Tensor, Y_prob: torch.Tensor, slices: int) -
 
     Y_prob_cpu = Y_prob.cpu()
     split = torch.split(Y_prob_cpu, sequence_length)
-    print(split)
-                 #il voto di maggioranza è eseguito così: si fa la media delle probabilità per 0 e la media delle probabilità per 1, invece che passare già dalle predicted labels
+    #print(split)
 
     for slice in split:
         slice = torch.mean(slice, dim=0)
@@ -190,14 +133,12 @@ def test_predict(Y_test, Y_prob, level: str = "slice", output_name: str = None, 
         test_array (np.Array): a numpy array containing 4 metrics computed on the predictions: roc, ACC, TPR, TNR. 
     """
     print(Y_test.shape)
-    #Y_test = Y_test.int()
     Y_test_cpu = Y_test.cpu()
     Y_prob_cpu = Y_prob.cpu()
-    #.argmax(axis=-1) significa che calcola il massimo rispetto all'ultimo asse Example: [[8,1],[2,33]] --> [0,1]
-    matrix = confusion_matrix(Y_test_cpu.argmax(axis=-1), Y_prob_cpu.argmax(axis=-1), labels=[0,1])
-    roc = roc_auc_score(Y_test_cpu.argmax(axis=-1), Y_prob_cpu[:, 1], labels=[0,1])
-    # matrix = confusion_matrix(torch.max(Y_test, dim=-1).indices, torch.max(Y_prob, dim=-1).indices, labels=[0,1])
-    # roc = roc_auc_score(torch.max(Y_test, dim=-1).indices, Y_prob[:, 1], labels=[0,1])
+    Y_prob_cpu = nn.Softmax(dim=1)(Y_prob_cpu)
+
+    matrix = confusion_matrix(Y_test_cpu.argmax(axis=-1), Y_prob_cpu.argmax(axis=-1)) #, labels=[0,1])
+    roc = roc_auc_score(Y_test_cpu.argmax(axis=-1), Y_prob_cpu[:, 1]) #, labels=[0,1])
 
     TN = matrix[0][0]
     FN = matrix[1][0]
@@ -205,43 +146,25 @@ def test_predict(Y_test, Y_prob, level: str = "slice", output_name: str = None, 
     FP = matrix[0][1]
 
     def compute_ratio_safe(x,y):
-        #try - non funziona, continua a scatenare l'error RuntimeWarning: invalid value encountered in scalar divide
         if (x+y)>0:
             z = (x/(x+y)).round(4)*100.0
         else:
-            z = 100.0
+            z = -10000
         return z
     
-    TPR = compute_ratio_safe(TP, FN) # Sensitivity, hit rate, recall, or true positive rate
-    TNR = compute_ratio_safe(TN, FP) # Specificity or true negative rate
-    PPV = compute_ratio_safe(TP, FP) # Precision or positive predictive value
-    NPV = compute_ratio_safe(TN, FN) # Negative predictive value
-    FPR = compute_ratio_safe(FP, TN) # Fall out or false positive rate
-    FNR = compute_ratio_safe(FN, TP) # False negative rate
-    FDR = compute_ratio_safe(FP, TP) # False discovery rate
+    TPR = compute_ratio_safe(TP, FN) # Sensitivity, hit rate, recall, or true positive rate    (TP / (TP + FN))
+    TNR = compute_ratio_safe(TN, FP) # Specificity or true negative rate                       (TN / (TN + FP))
+    PPV = compute_ratio_safe(TP, FP) # Precision or positive predictive value                  (TP / (TP + FP))
+    NPV = compute_ratio_safe(TN, FN) # Negative predictive value                               (TN / (TN + FN))
+    FPR = compute_ratio_safe(FP, TN) # Fall out or false positive rate                         (FP / (FP + TN))
+    FNR = compute_ratio_safe(FN, TP) # False negative rate                                     (FN / (FN + TP))
+    FDR = compute_ratio_safe(FP, TP) # False discovery rate                                    (FP / (FP + TP))
+    F1_SCORE = (2*PPV*TPR) / (PPV + TPR)
     
     ACC = ((TP + TN) / (TP + FP + FN + TN)).round(4) * 100.0 # Overall accuracy
 
-    # # Sensitivity, hit rate, recall, or true positive rate
-    # TPR = (TP / (TP + FN)).round(4) * 100.0
-    # # Specificity or true negative rate
-    # TNR = (TN / (TN + FP)).round(4) * 100.0
-    # # Precision or positive predictive value
-    # PPV = (TP / (TP + FP)).round(4) * 100.0
-    # # Negative predictive value
-    # NPV = (TN / (TN + FN)).round(4) * 100.0
-    # # Fall out or false positive rate
-    # FPR = (FP / (FP + TN)).round(4) * 100.0
-    # # False negative rate
-    # FNR = (FN / (FN + TP)).round(4) * 100.0
-    # # False discovery rate
-    # FDR = (FP / (FP + TP)).round(4) * 100.0
-    # # Overall accuracy
-    # ACC = ((TP + TN) / (TP + FP + FN + TN)).round(4) * 100.0
-
     # ROC plot
     FPR_AUC, TPR_AUC, thresholds = roc_curve(Y_test_cpu.argmax(axis=-1), Y_prob_cpu[:, 1])
-    #FPR_AUC, TPR_AUC, thresholds = roc_curve(torch.max(Y_test, dim=-1).indices, Y_prob[:, 1])
    
     trace_roc = go.Scatter(x=FPR_AUC, y=TPR_AUC, mode='lines', name="Model ROC (AUC={0:.2f})".format(roc))
     trace_random = go.Scatter(x=[0, 1], y=[0, 1], mode='lines', name='random guess', line=dict(dash='dash'))
@@ -267,24 +190,19 @@ def test_predict(Y_test, Y_prob, level: str = "slice", output_name: str = None, 
         print("\nPATIENT LEVEL")
     print(output_name, "\n")
 
-    pred_labels = "Fold: {0}\nModel: {1}\nReal labels: {2}\nPred labels: {3}\n".format(fold + 1, output_name,
-                                                                                      #  Y_test.argmax(axis=-1),
-                                                                                      #  Y_prob.argmax(axis=-1))
-                                                                                      torch.max(Y_test, dim=-1).indices,
-                                                                                      torch.max(Y_prob, dim=-1).indices)
-    confusion_matrix_string = "Fold: {0}\nLevel: {1}\nModel: {2}\nTN: {3}\nFN: {4}\nTP: {5}\nFP: {6}\n".format(fold + 1,
-                                                                                                               level,
-                                                                                                               output_name,
-                                                                                                               TN, FN,
-                                                                                                               TP, FP)
-    confusion_matrix_deriv = "Fold: {0}\nLevel: {1}\nModel: {2}\n\nAccuracy: {3:.2f}%\nSensitivity (TPR): {3:.2f}%\nSpecificity (TNR): {5:.2f}%\nFall-out (FPR): {6:.2f}%\nFalse Negative Rate (FNR): {7:.2f}%\nPrecision (PPV): {8:.2f}%\nNegative Predictive Value (NPV): {9:.2f}%".format(
-        fold + 1, level, output_name, ACC, TPR, TNR, FPR, FNR, PPV, NPV)
+    intro_label = f"Fold: {fold+1} - Model: {output_name} - Level: {level}\n"
+    pred_labels = "Real labels: {0}\nPred labels: {1}\n".format(torch.max(Y_test, dim=-1).indices, torch.max(Y_prob, dim=-1).indices)
+                                                     
+    confusion_matrix_string = "TN: {0}\nFN: {1}\nTP: {2}\nFP: {3}\n".format(TN, FN,TP, FP)
 
+    confusion_matrix_deriv = "Accuracy: {0:.2f}%\nF1-Score: {7:.2f}%\nSensitivity (TPR): {1:.2f}%\nSpecificity (TNR): {2:.2f}%\nFall-out (FPR): {3:.2f}%\nFalse Negative Rate (FNR): {4:.2f}%\nPrecision (PPV): {5:.2f}%\nNegative Predictive Value (NPV): {6:.2f}%".format(
+        ACC, TPR, TNR, FPR, FNR, PPV, NPV, F1_SCORE)
+
+    print(intro_label)
     print(pred_labels)
     print(confusion_matrix_string)
     print(confusion_matrix_deriv)
-
     print("Val AUC: {0:.2f}\n".format(roc))
 
-    test_array = [roc, ACC, TPR, TNR]
+    test_array = [roc, ACC, TPR, TNR, F1_SCORE]
     return np.array(test_array)

utils/train_utils.py
@@ -1,155 +1,54 @@
-import os
-import ast
-import SimpleITK as sitk
+import torch
+import logging
 import numpy as np
-import torch 
+from torchvision.transforms import v2 as T
 
-Kfold_val = [[10, 13, 18, 22, 28, 31, 32, 37, 4], #5, 29 missing - 31 double
-              [1, 12, 14, 16, 19, 26, 33, 35, 9],
-              [15, 17, 2, 20, 24, 27, 3, 30, 7],
-              [11, 21, 23, 25, 31, 34, 36, 6, 8]]
+def get_transformations():
+    transforms = T.Compose([
+            T.RandomResizedCrop(size=(224, 224), scale=(1,1), antialias=True), #interpolation=InterpolationMode.NEAREST,
+            T.RandomVerticalFlip( p = 0.5),
+            T.ColorJitter( brightness = (0.5 , 2)),
+            T.RandomAffine(
+                degrees = 0,
+                scale =(0.7, 1.3),
+                shear = 0.3
+            ),
+            T.ToImage(),
+            T.ToDtype(torch.float32, scale=True),
+            #T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) --> normalization on ImageNet dataset.
+        ])
+    return transforms
 
-# Kfold_val = [[10, 2, 11],   #solo per debugging
-#              [3,11],
-#              [2],
-#              [1]]
+def get_val_transformations():
+    transforms = T.Compose([
+            T.ToImage(),
+            T.ToDtype(torch.float32, scale=True),
+            #T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) --> normalization on ImageNet dataset.
+        ])
+    return transforms
 
-def retrieve_folders_list(root_dir:str) -> "list[str]":
-        """
-        Scan the root dir and retrieve all the individual folders containing the DICOM images.
-        Params:
-            root_dir(str): the path of the root directory.
-        Returns:
-            train_folders(list[str]): list with the paths of the folders containing a sequence each.
-        """
-        train_folders=[]
-        for root, dirs, files in os.walk(root_dir):
-            dirs.sort()
-            for folder in dirs:
-                if folder[:3] != 'NAC' and folder != 'MRI_1' and folder != 'MRI_2':  #excluding all the repos that are not the final ones
-                    train_folders.append(os.path.join(root,folder))
-        return train_folders
 
-def Kfold_split(folders, k):
+def normalize_slice_channelwise(slice: torch.Tensor)-> torch.Tensor:
     """
-    Splits the folders at patient level, in order to have train and validation dataset.
-    Arguments:
-        folders (list[str]): list of folder paths.
-        k (int): number of kfolds.
-    Returns:
-        Kfold_list (list[list[str]]): a list containing two lists, train and validation lists. 
+    Apply normalization to a single 3-channels image.
     """
-    old_patient = ""
-    patient_sequences= []
-    patient_list = []
-    Kfold_list = []
+    std,mean = torch.std_mean(slice, dim=(-2,-1))
+    #logging.info(f"Mean: {mean} | STD: {std}")
+    normalization = T.Compose([
+        T.Normalize(mean=mean, std=std)
+    ])
+    normalized_slice = normalization(slice)
+    #std_n,mean_n = torch.std_mean(normalized_slice, dim=(-2,-1))
+    #logging.info(f"Post normalization:       Mean: {mean_n} | STD: {std_n}")
+    return normalized_slice
 
-    for sequence in folders:
-        patient_flag = False
-        dicom_files = []
-
-        for r, d, f in os.walk(sequence):
-            f.sort()
-            for file in f:
-                if '.dcm' in file or '.IMA' in file:
-                    dicom_files.append(os.path.abspath(os.path.join(r,file)))
-                    break
-
-        reader = sitk.ImageFileReader()
-        reader.SetFileName(dicom_files[0])
-        reader.LoadPrivateTagsOn()
-        reader.ReadImageInformation()
-
-        description = reader.GetMetaData("0008|103e").strip()
-
-        name_num = description.split('_')[0]
-
-        if name_num == old_patient:
-            patient_sequences.append(sequence)
-        else:
-            old_patient = name_num
-            patient_flag = True
-            patient_sequences= []
-            patient_list.append(patient_sequences)
-            patient_sequences.append(sequence)
-
-    for fold in range(k):
-        train_list = []
-        val_list = []
-        fold_list = []
-
-        nac_train_name = []
-        nac_val_name = []
-
-        for patient in patient_list:
-            dicom_files = []
-
-            for r, d, f in os.walk(patient[0]):
-                f.sort()
-                for file in f:
-                    if '.dcm' in file or '.IMA' in file:
-                        dicom_files.append(os.path.abspath(os.path.join(r,file)))
-                        break
-
-            reader.SetFileName(dicom_files[0])
-            reader.LoadPrivateTagsOn()
-            reader.ReadImageInformation()
-
-            description = reader.GetMetaData("0008|103e").strip()
-
-            name_num = int(description.split('_')[0])
-
-            if name_num in Kfold_val[fold]: #qua decide quali pazienti finiscono nel train e quali nel val
-                val_list.extend(patient)
-                nac_val_name.append(str(name_num))
-            else:
-                train_list.extend(patient)
-                nac_train_name.append(str(name_num))
-                
-        print(f"Fold number {fold+1}:")
-        print(f"Training patients: {set(nac_train_name)}")
-        print(f"Validation patients: {set(nac_val_name)}\n")
-
-        fold_list.append(train_list)
-        fold_list.append(val_list)
-        Kfold_list.append(fold_list)
-
-    return Kfold_list
-
-def print_results(roc_slice, roc_patient):
-    print("Printo i results")
-    log_list = ['auc', 'accuracy', 'sensitivity', 'specificity']
-    for key in roc_slice[0].keys():
-        
-        std_array_patient = []
-        if key == 'pCR':
-            for d in roc_patient:
-                std_array_patient.append(d[key][0])
-        mean_value = sum(d[key] for d in roc_patient) / len(roc_patient)
-        std_value = np.std(std_array_patient)
-        for i, log in enumerate(log_list):
-            if log == 'auc' and key == 'pCR':
-                #BOH
-                pass
-            str_value = '{0}_patient-level {1} mean = {2}'.format(log, key, mean_value[i])
-            print(str_value)
-            if log == 'auc' and key == 'pCR':
-                str_std_value = '{0}_patient-level {1} std = {2}'.format(log, key, std_value)
-                print(str_std_value)
-
-        std_array_slice = []
-        if key == 'pCR':
-            for d in roc_slice:
-                std_array_slice.append(d[key][0])
-        mean_value = sum(d[key] for d in roc_slice) / len(roc_slice)
-        std_value = np.std(std_array_slice)
-        for i, log in enumerate(log_list):
-            if log == 'auc' and key == 'pCR':
-                #BOH
-                pass
-            str_value = '{0}_slice-level {1} mean = {2}'.format(log, key, mean_value[i])
-            print(str_value)
-            if log == 'auc' and key == 'pCR':
-                str_std_value = '{0}_slice-level {1} std = {2}'.format(log, key, std_value)
-                print(str_std_value)
-    print("Piccolo scoiattolo")
+def normalize_slices(t1: torch.Tensor)-> torch.Tensor:
+    """
+    This method apply slice-wise normalization to the input batch.
+    Expected tensor input shape: [BATCH_SIZE, NUM_MODALITIES, C, H, W]
+    """
+    X = torch.Tensor()
+    for modality in range(t1.shape[0]):
+      slice_normalized = normalize_slice_channelwise(t1[modality])
+      X = torch.cat((X, slice_normalized.unsqueeze(0)))
+    return X

visualize.py
@@ -10,6 +10,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from datetime import datetime
 
+import logging
+logging.basicConfig(level=logging.INFO)
+
 torch.manual_seed(42)
 np.random.seed(42)
 
@@ -18,17 +21,26 @@ def get_transformations(choice: int = 1) -> "torchvision.transforms":
         transforms = T.Compose([
             T.RandomResizedCrop(size=(224, 224), scale=(1,1), antialias=True),
             T.RandomHorizontalFlip( p = 0.5),
-            T.ColorJitter( brightness = (0.5 , 3)),
+            T.ColorJitter( brightness = (0.5 , 2)), #brightness troppo alta considerando la compressione bit -> provare a riddure max value a 2
             T.RandomAffine(
                 degrees = 0,
                 scale =(0.7, 1.3),
                 shear = 0.3
             ),
-            T.ToTensor()
+            #T.ToTensor()
         ])
     else:
         transforms = None
-    return transforms
+    return transforms              #per visualizzare post norm devi riscalare tra min max e poi x 255
+
+def save_images(images, output_name):
+    grid = torchvision.utils.make_grid(images.view(-1,3,224,224))
+    ndarr = grid.permute(1, 2, 0).cpu().numpy()
+    plt.figure(figsize=(10,10))
+    plt.imshow(ndarr)
+    plt.axis('off')  # Turn off axis
+    plt.savefig(output_name, bbox_inches='tight', pad_inches=0)
+    plt.close()
 
 # def clip_into_0_255(original_tensor):
 #     """
@@ -41,30 +53,43 @@ def get_transformations(choice: int = 1) -> "torchvision.transforms":
 
 if __name__ == "__main__":
     index_to_print = [9,22,27] #[9,22,148]
-    full_path_to_dataset = ""
+    time_run = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    slices = 3
+    full_path_to_dataset = "C:\\Users\\c.navilli\\Desktop\\Prova\\dataset_mini"
+    preprocess_type="percentile"                        #choices for preprocess_type = min_max, norm, 10bit, 12bit, 16bit, norm_and_scale. Defaults to norm without scale
+    
+    if preprocess_type in ["16bit"]:
+        multiply_255=True
+    else:
+        multiply_255=False
+
     folders_list = retrieve_folders_list(full_path_to_dataset)
     datasets_list = Kfold_split(folders_list, 1)
-    print(len(datasets_list[0]))
-    #training_list = datasets_list[0]
-    #validation_list = datasets_list[1]
-    slices = 3
-    transformations = get_transformations()
-
-    preprocess_type="norm_and_scale" #choices for preprocess_type = min_max, norm, 10bit, 12bit, 16bit, norm_and_scale. Defaults to norm without scale
+    
     dataset = MRIDataset(datasets_list[0][0], slices, preprocess_type=preprocess_type) 
 
-    time_run = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    transformations = get_transformations() 
 
     for index in index_to_print:
         print(index)
+        image_file_name = f'2605_2_image_scale_{preprocess_type}_{index}.png'
         images, _ = dataset.__getitem__(index)
+        if multiply_255:
+            images = images *255
+            image_file_name = image_file_name.split(".")[0]+"_255"+".png"
+        
+        images_for_visualization = images #/(2**16-1)*255 #---> perchè così saranno compresi tra 0 e 1, come si aspetta la visualizzazione per dati di tipo float
+        # if index==9:
+        #     print(images)
         transformed_images = transformations(images)
-        comparison_images = torch.cat((images.unsqueeze(0), transformed_images.unsqueeze(0)))
+        print(f"Min pre transformation: {images.min()} - Max pre transformation: {images.max()}")
+        print(f"Min post transformation: {transformed_images.min()} - Max post transformation: {transformed_images.max()}")
+        
+        #transformed_for_visualization = (transformed_images*255).astype(np.int8) #(perchè di output da transformations sono tutte clippate nel range 0-1)
+        #print(f"Min post viz adjustement: {transformed_for_visualization.min()} - Max post viz adjustement: {transformed_for_visualization.max()}")
 
-        grid = torchvision.utils.make_grid(comparison_images.view(-1,3,224,224))
-        ndarr = grid.permute(1, 2, 0).cpu().numpy()
-        plt.figure(figsize=(10,10))
-        plt.imshow(ndarr)
-        plt.axis('off')  # Turn off axis
-        plt.savefig(f'image_scale_{preprocess_type}_{index}.png', bbox_inches='tight', pad_inches=0)
-        plt.close()
+        comparison_images = torch.cat((images_for_visualization.unsqueeze(0), transformed_images.unsqueeze(0)))
+
+        save_images(comparison_images, image_file_name)
+
+        